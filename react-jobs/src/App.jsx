96084a273e3cec107256297cb72c3d2512719b35
mbuguajackson <jak.mbugua@gmail.com>
2025-06-03 15:32:58 +0300

    NavLink>

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -3,11 +3,16 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import React from 'react'
 import HomePage from './pages/HomePage.jsx'
 import MainLayout from './layouts/MainLayout.jsx'
+import JobsPage from './pages/JobsPage.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+      
     </Route>
   )
 );
--- react-jobs/src/pages/HomePage.jsx
+++ react-jobs/src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero.jsx'
+import HomeCards from '../components/HomeCards.jsx'
+import JobListings from '../components/JobListings.jsx'
+import ViewAllJobs from '../components/ViewAllJobs.jsx'
 
 const HomePage = () => {
     return (
         <>
             <Hero />
+            <HomeCards />
+            <JobListings />
+            <ViewAllJobs />
         </>
     )
 }
