b957cd17785d4b3b54b5e24c073c634e4e59af25
mbuguajackson <jak.mbugua@gmail.com>
2025-06-03 23:06:01 +0300

    spinners

--- react-jobs/src/components/JobListings.jsx
+++ react-jobs/src/components/JobListings.jsx
@@ -13,8 +13,10 @@ const JobListings = ({ isHome = false }) => {
     // useEffect to fetch jobs
     useEffect(() => {
         const fetchJobs = async () => {
+            // limit jobs
+            const apiUrl = isHome ? 'http://localhost:5000/jobs?_limit=3' : 'http://localhost:5000/jobs';
             try {
-                const res = await fetch('http://localhost:5000/jobs')
+                const res = await fetch(apiUrl);
                 const data = await res.json();
                 setJobs(data);
             } catch (error) {
@@ -35,18 +37,17 @@ const JobListings = ({ isHome = false }) => {
                     <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                         {isHome ? 'Recent Jobs' : 'Browse Jobs'}
                     </h2>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        {loading ? (<Spinner loading={loading} />)
-                            : (
-                                <>
-                                    {jobs.map((job) => (
-                                        <JobListing key={job.id} job={job} />
-                                    ))}
-                                </>
-                            )}
+
+                    {loading ? (<Spinner loading={loading} />)
+                        : (
+                            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                                {jobs.map((job) => (
+                                    <JobListing key={job.id} job={job} />
+                                ))}
+                            </div>
+                        )}
 
 
-                    </div>
                 </div>
             </section>
         </div>
