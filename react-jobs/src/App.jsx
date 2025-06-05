4d60c3755f181b9e38d0418ffeef25434989fa6e
mbuguajackson <jak.mbugua@gmail.com>
2025-06-05 10:02:38 +0300

    JobPage

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -5,12 +5,17 @@ import HomePage from './pages/HomePage.jsx'
 import MainLayout from './layouts/MainLayout.jsx'
 import JobsPage from './pages/JobsPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
+import JobPage, {jobLoader} from './pages/JobPage.jsx'
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+      
+      {/* Catch-all route for 404 Not Found */}
       <Route path='*' element={<NotFoundPage />} />
       
     </Route>
