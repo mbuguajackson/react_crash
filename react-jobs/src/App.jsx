3ca8ef98f20ac1354a5aaf5da9a68159e6905d61
mbuguajackson <jak.mbugua@gmail.com>
2025-06-06 17:33:05 +0300

    complete

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -5,24 +5,64 @@ import HomePage from './pages/HomePage.jsx'
 import MainLayout from './layouts/MainLayout.jsx'
 import JobsPage from './pages/JobsPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
-import JobPage, {jobLoader} from './pages/JobPage.jsx'
+import JobPage, { jobLoader } from './pages/JobPage.jsx'
+import AddJobPage from './pages/AddJobPage.jsx'
+import EditJobPage from './pages/EditJobPage.jsx'
 
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
-      
-      {/* Catch-all route for 404 Not Found */}
-      <Route path='*' element={<NotFoundPage />} />
-      
-    </Route>
-  )
-);
 
 const App = () => {
+
+  //add new job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+
+    });
+    return;
+  };
+  // Delete job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE'
+    });
+    return;
+  };
+
+  // Update job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+
+    });
+    return;
+  };
+
+
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob} />} loader={jobLoader} />
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+
+        {/* Catch-all route for 404 Not Found */}
+        <Route path='*' element={<NotFoundPage />} />
+
+      </Route>
+    )
+  );
   return <RouterProvider router={router} />
 
 
--- react-jobs/src/pages/JobPage.jsx
+++ react-jobs/src/pages/JobPage.jsx
@@ -2,15 +2,31 @@
 import React from 'react'
 import { useParams, useLoaderData, Link } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
   // use params to get job id
   const { id } = useParams();
+  const navigate = useNavigate();
 
   // Use loadeer data to fetch job details
   const job = useLoaderData();
+  const onDeleteClick = (JobId) => {
+    const confirrm = window.confirm('Are you sure you want to delete this job?');
+
+
+    if (!confirm) return;
+    deleteJob(JobId);
+    toast.success('Job deleted successfully');
+    // redirect to jobs page
+    navigate('/jobs');
+
+  };
+
+
 
 
 
@@ -22,7 +38,7 @@ const JobPage = () => {
             to="/jobs"
             className="text-indigo-500 hover:text-indigo-600 flex items-center"
           >
-            <FaArrowLeft className='mr-2'/> Back to Job Listings
+            <FaArrowLeft className='mr-2' /> Back to Job Listings
           </Link>
         </div>
       </section>
@@ -36,7 +52,7 @@ const JobPage = () => {
               >
                 <div className="text-gray-500 mb-4"> {job.type} </div>
                 <h1 className="text-3xl font-bold mb-4">
-                  { job.title}
+                  {job.title}
                 </h1>
                 <div
                   className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
@@ -67,10 +83,10 @@ const JobPage = () => {
               <div className="bg-white p-6 rounded-lg shadow-md">
                 <h3 className="text-xl font-bold mb-6">Company Info</h3>
 
-                <h2 className="text-2xl">NewTek Solutions</h2>
+                <h2 className="text-2xl">{job.company.name}</h2>
 
                 <p className="my-2">
-                  NewTek Solutions is Link leading technology company specializing in web development and digital solutions. We pride ourselves on delivering high-quality products and services to our clients while fostering Link collaborative and innovative work environment.
+                  {job.company.description}
                 </p>
 
                 <hr className="my-4" />
@@ -78,23 +94,23 @@ const JobPage = () => {
                 <h3 className="text-xl">Contact Email:</h3>
 
                 <p className="my-2 bg-indigo-100 p-2 font-bold">
-                  contact@newteksolutions.com
+                  {job.company.contactEmail}
                 </p>
 
                 <h3 className="text-xl">Contact Phone:</h3>
 
-                <p className="my-2 bg-indigo-100 p-2 font-bold">555-555-5555</p>
+                <p className="my-2 bg-indigo-100 p-2 font-bold">{job.company.contactPhone}</p>
               </div>
 
               {/* <!-- Manage --> */}
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to="/add-job.html"
+                  to={`/edit-job/${id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</Link
                 >
-                <button
+                <button onClick={() => onDeleteClick(job.id)}
                   className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Delete Job
